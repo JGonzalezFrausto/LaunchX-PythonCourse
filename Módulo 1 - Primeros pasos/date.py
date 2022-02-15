d61373be2fa877bc682dac6ba0ba65eb3b990821
Javier Alejandro Gonzalez <alejandro.gonzalez@edrsilver.com>
2022-02-14 21:21:47 -0600

    first commit

---
+++ Módulo 1 - Primeros pasos/date.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+
+print("Today's date is: " + str(date.today()))
+
+print("Buenvenido al programa de bienvenida")
+name = input("Intoduzca su nombre ")
+print("Saludos "+name)
+
+
+print("Calculadora")
+first_number = input("Introduce el primer numero: ")
+second_number= input("Introduce el segundo número: ")
+print( int(first_number) + int(second_number))
